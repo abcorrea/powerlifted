7bcbb281af607194c18b8fc0bb9531d43004ae5d
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-06-25 11:22:05 +0200

    Remove TODO and reformat

--- src/translator/translate.py
+++ src/translator/translate.py
@@ -13,9 +13,9 @@ def python_version_supported():
     major, minor = sys.version_info[:2]
     return (major == 2 and minor >= 7) or (major, minor) >= (3, 2)
 
+
 if not python_version_supported():
     sys.exit("Error: Translator only supports Python >= 2.7 and Python >= 3.2.")
-
 
 from collections import defaultdict
 from copy import deepcopy
@@ -33,18 +33,18 @@ import tools
 
 DEBUG = False
 
-
 ## For a full list of exit codes, please see driver/returncodes.py. Here,
 ## we only list codes that are used by the translator component of the planner.
 TRANSLATE_OUT_OF_MEMORY = 20
 TRANSLATE_OUT_OF_TIME = 21
+
 
 def main():
     timer = timers.Timer()
     with timers.timing("Parsing", True):
         task = pddl_parser.open(
             domain_filename=options.domain, task_filename=options.task)
-    print ('Processing task', task.task_name)
+    print('Processing task', task.task_name)
     with timers.timing("Normalizing task"):
         normalize.normalize(task)
 
@@ -54,13 +54,12 @@ def main():
     with timers.timing("Checking static predicates"):
         static_predicates.check(task)
 
-    # TODO:
-    #  - Check that complete initial state is actually correct.  Numbers do not match for Logistics98 task 25
     with timers.timing("Generating complete initial state"):
         complete_state.generate_complete_initial_state(task, g)
 
-    print ("Initial state length:", len(task.init))
-    #task.dump()
+    print("Initial state length:", len(task.init))
+    # task.dump()
+
 
 def handle_sigxcpu(signum, stackframe):
     print()
@@ -75,13 +74,13 @@ if __name__ == "__main__":
         signal.signal(signal.SIGXCPU, handle_sigxcpu)
     except AttributeError:
         print("Warning! SIGXCPU is not available on your platform. "
-            "This means that the planner cannot be gracefully terminated "
-            "when using a time limit, which, however, is probably "
-            "supported on your platform anyway.")
+              "This means that the planner cannot be gracefully terminated "
+              "when using a time limit, which, however, is probably "
+              "supported on your platform anyway.")
     try:
         # Reserve about 10 MB (in Python 2) of emergency memory.
         # https://stackoverflow.com/questions/19469608/
-        emergency_memory = "x" * 10**7
+        emergency_memory = "x" * 10 ** 7
         main()
     except MemoryError:
         emergency_memory = ""
