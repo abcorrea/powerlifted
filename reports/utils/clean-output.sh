b56003de073f168cb6aa3a9d4786391caae182e5
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-06-26 13:47:51 +0200

    Add util file to clean translator output

---
+++ reports/utils/clean-output.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Gets a file in the format
+#
+# > DOMAIN PROBLEM : initial state size XX : time [YYYs CPU, ZZZs wall-clock]
+#
+# and parses it into a file
+#
+# > domain problem state_size cpu_time user_time
+# > DOMAIN PROBLEM XX         YYY      ZZZ
+# > ...
+#
+# Usage
+# $ ./clean-output.sh INPUT_FILE
+#
+
+while IFS= read -r line; do
+    echo $line | sed "s/: initial state size//" | sed "s/: time \[//" | sed "s/s CPU,//" | sed "s/s wall-clock\]//" | column -t
+done < "$1"
