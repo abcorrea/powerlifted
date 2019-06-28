942347cee83122ce7f535f54ad8b94b8fe4efe23
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-06-28 18:39:56 +0200

    Start search component

---
+++ src/search/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    for (int cont = 0; cont < 5; ++cont) {
+        cout << cont << endl;
+    }
+    
+    return 0;
+}
