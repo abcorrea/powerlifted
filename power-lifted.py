2f20fca703acd8325ec91d9aebd04ef8ca111d04
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-08-01 21:01:07 +0200

    Change arguments for driver script

--- power-lifted.py
+++ power-lifted.py
@@ -5,17 +5,32 @@ import argparse
 import os
 import subprocess
 
+from distutils.dir_util import copy_tree
+from shutil import copyfile
+
 def parse_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--translator-output-file', dest='translator_file',
-                        help='Output file for the translator')
-    parser.add_argument('--domain', dest='domain', action='store', default=None,
-                        help='Domain file in PDDL')
-    parser.add_argument('--instance', dest='instance', action='store', default=None,
-                        help='instance file in PDDL')
+    parser.add_argument('--translator-output-file',
+                        dest='translator_file',
+                        default='output.lifted',
+                        help='Output file of the translator')
+    parser.add_argument('--domain', dest='domain', action='store',
+                        default=None, help='Domain file in PDDL')
+    parser.add_argument('--instance', dest='instance', action='store',
+                        default=None, help='Instance file in PDDL')
     return parser.parse_args()
 
 if __name__ == '__main__':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     options = parse_options()
-    subprocess.check_call(['/home/blaas/thesis/planner/src/translator/translate.py',
-                           options.domain, options.instance])
+    os.mkdir(dir_path+'/builds')
+    os.mkdir(dir_path+'/builds/release')
+    os.mkdir(dir_path+'/builds/release/src')
+    os.chdir(dir_path+'/builds/release/src')
+    copy_tree(dir_path+'/src/translator/', dir_path+'/builds/release/translator')
+    subprocess.check_call(['cmake', dir_path+'/src/search/'])
+    subprocess.check_call(['make'])
+    os.chdir(dir_path)
+    subprocess.check_call(['./src/translator/translate.py',
+                           options.domain, options.instance,
+                           '--output-file', options.translator_file])
