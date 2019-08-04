4f5dd82b326705fd2802c82db3dec423b1a82a34
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-08-04 14:20:54 +0200

    finish driver script

--- power-lifted.py
+++ power-lifted.py
@@ -10,27 +10,46 @@ from shutil import copyfile
 
 def parse_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--translator-output-file',
-                        dest='translator_file',
+    parser.add_argument('-d', '--domain', dest='domain', action='store',
+                                  default=None, help='Domain file in PDDL', required=True)
+    parser.add_argument('-i', '--instance', dest='instance',
+                        action='store', default=None,
+                        help='Instance file in PDDL', required=True)
+    parser.add_argument('-s', '--search', dest='search', action='store',
+                        default=None, help='Search algorithm',
+                        required=True)
+    parser.add_argument('-e', '--heuristic', dest='heuristic', action='store',
+                        default=None,
+                        help='Heuristic to guide the search (ignore in case of blind search)',
+                        required=True)
+    parser.add_argument('-g', '--generator', dest='generator', action='store',
+                        default=None, help='Successor generator method',
+                        required=True)
+    parser.add_argument('--translator-output-file', dest='translator_file',
                         default='output.lifted',
                         help='Output file of the translator')
-    parser.add_argument('--domain', dest='domain', action='store',
-                        default=None, help='Domain file in PDDL')
-    parser.add_argument('--instance', dest='instance', action='store',
-                        default=None, help='Instance file in PDDL')
     return parser.parse_args()
 
 if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     options = parse_options()
-    os.mkdir(dir_path+'/builds')
-    os.mkdir(dir_path+'/builds/release')
-    os.mkdir(dir_path+'/builds/release/src')
-    os.chdir(dir_path+'/builds/release/src')
+
+
+    # Create build path
+    if not os.path.exists(dir_path+'/builds/release/search'):
+        os.makedirs(dir_path+'/builds/release/search')
+
     copy_tree(dir_path+'/src/translator/', dir_path+'/builds/release/translator')
+    os.chdir(dir_path+'/builds/release/search')
     subprocess.check_call(['cmake', dir_path+'/src/search/'])
     subprocess.check_call(['make'])
+    
     os.chdir(dir_path)
-    subprocess.check_call(['./src/translator/translate.py',
+    subprocess.check_call([dir_path+'/builds/release/translator/translate.py',
                            options.domain, options.instance,
                            '--output-file', options.translator_file])
+    subprocess.check_call([dir_path+'/builds/release/search/search',
+                           options.translator_file,
+                           options.search,
+                           options.heuristic,
+                           options.generator])
--- reports/experiments/lab-test/common_setup.py
+++ reports/experiments/lab-test/common_setup.py
@@ -4,3 +4,8 @@ class Configuration:
     def __init__(self, name, arguments):
         self.name = name
         self.arguments = arguments
+
+    def __str__(self):
+        return self.name
+
+    
