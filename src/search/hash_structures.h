10732705dcdc8af61641aa19bb63132b4201fbe3
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-08-19 18:02:00 +0200

    Add doxygen to hash functions

--- src/search/hash_structures.h
+++ src/search/hash_structures.h
@@ -5,6 +5,12 @@
 
 #include <boost/functional/hash/hash.hpp>
 
+/**
+ * @brief Generic hash function for vector of integers. The name "TupleHash" is used because its
+ * main function is to hash tuples in relations/tables.
+ *
+ * @note Uses boost hash_range function
+ */
 struct TupleHash {
     std::size_t operator() (const std::vector<int> &c) const {
         return boost::hash_range(c.begin(), c.end());
