cd4c856a3ba2166449bd88e992e19f31febc5982
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2019-10-22 08:25:18 +0200

    add script to combine experiments

---
+++ reports/experiments/yannakakis/combine-blind.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import os
+import platform
+
+from suites import OPTIMAL_SUITE, EXCLUDED_DOMAINS, CYCLIC_SCHEMAS
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from lab.experiment import Experiment
+
+from downward import suites
+from downward.reports.absolute import AbsoluteReport
+from downward.reports.scatter import ScatterPlotReport
+
+from common_setup import Configuration
+from exp_utils import *
+
+
+def memory_filter(run):
+    if 'peak_memory' in run:
+        run['memory'] = run['peak_memory']
+    else:
+        run['memory'] = None
+    return run
+
+def time_filter(run):
+    if 'total_time' not in run:
+        if 'search_time' in run:
+            run['total_time'] = run['search_time']
+    return run
+
+exp = Experiment('data/combine-blind-eval')
+exp.add_fetcher('data/v2-eval')
+exp.add_fetcher('/infai/blaas/issues/issue311/experiments/thesis/data/thesis-v1-eval')
+exp.add_fetcher('data/goalcount-v1-eval')
+exp.add_report(AbsoluteReport(attributes=['cost', 'coverage', 'generated', 'total_time', 'memory'], filter=[memory_filter, time_filter]))
+
+exp.run_steps()
