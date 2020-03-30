e4cb24866c676b2eb374a2597bf2e85c940ca62d
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-03-30 17:46:04 +0200

    Refactor goalcount heuristic to extract functions
      - Extract function to compute nullary atom goalcount
      - Extract function to compute if atom is not satisfied

--- src/search/heuristics/goalcount.cc
+++ src/search/heuristics/goalcount.cc
@@ -4,40 +4,55 @@
 #include "../task.h"
 
 int Goalcount::compute_heuristic(const State &s, const Task &task) {
-  /*
-   * First, loop over all nullary atoms.  Then loop over all arguments in the
-   * goal condition and checks which ones are satisfied or not.
-   *
-   */
-  int h = 0;
-  for (int pred : task.goal.positive_nullary_goals) {
-    if (!s.nullary_atoms[pred])
-      h++;
-  }
-  for (int pred : task.goal.negative_nullary_goals) {
-    if (!s.nullary_atoms[pred])
-      h++;
-  }
-  for (const AtomicGoal &atomicGoal : task.goal.goal) {
-    assert(atomicGoal.predicate ==
-           s.relations[atomicGoal.predicate].predicate_symbol);
-    if (!atomicGoal.negated) {
-      // Positive goal
-      if (find(s.relations[atomicGoal.predicate].tuples.begin(),
-               s.relations[atomicGoal.predicate].tuples.end(),
-               atomicGoal.args) ==
-          s.relations[atomicGoal.predicate].tuples.end()) {
-        h++;
-      }
-    } else {
-      // Negative goal
-      if (find(s.relations[atomicGoal.predicate].tuples.begin(),
-               s.relations[atomicGoal.predicate].tuples.end(),
-               atomicGoal.args) !=
-          s.relations[atomicGoal.predicate].tuples.end()) {
-        h++;
-      }
+    /*
+     * First, loop over all nullary atoms.  Then loop over all arguments in the
+     * goal condition and checks which ones are satisfied or not.
+     *
+     */
+    int h = compute_reached_nullary_atoms(task.goal.positive_nullary_goals,
+                                          s.nullary_atoms);
+    h += compute_reached_nullary_atoms(task.goal.negative_nullary_goals,
+                                       s.nullary_atoms);;
+
+    for (const AtomicGoal &atomicGoal : task.goal.goal) {
+        assert(atomicGoal.predicate==
+            s.relations[atomicGoal.predicate].predicate_symbol);
+        h += atom_not_satisfied(s, atomicGoal);
     }
-  }
-  return h;
+    return h;
+}
+
+
+int Goalcount::atom_not_satisfied(const State &s,
+                                  const AtomicGoal &atomicGoal) const {
+    if (!atomicGoal.negated) {
+        // Positive goal
+        if (find(s.relations[atomicGoal.predicate].tuples.begin(),
+                 s.relations[atomicGoal.predicate].tuples.end(),
+                 atomicGoal.args)==
+            s.relations[atomicGoal.predicate].tuples.end()) {
+            return 1;
+        }
+    }
+    else {
+        // Negative goal
+        if (find(s.relations[atomicGoal.predicate].tuples.begin(),
+                 s.relations[atomicGoal.predicate].tuples.end(),
+                 atomicGoal.args)!=
+            s.relations[atomicGoal.predicate].tuples.end()) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int
+Goalcount::compute_reached_nullary_atoms(const std::unordered_set<int> &indices,
+                                         const std::vector<bool> &nullary_atoms) {
+    int h = 0;
+    for (int pred : indices) {
+        if (!nullary_atoms[pred])
+            h++;
+    }
+    return h;
 }
--- src/search/heuristics/goalcount.h
+++ src/search/heuristics/goalcount.h
@@ -14,8 +14,13 @@
  */
 class Goalcount : public Heuristic {
 public:
-    int compute_heuristic(const State &s, const Task &task) final;
-};
+    int compute_heuristic(const State& s, const Task& task) final;
 
+private:
+  static int compute_reached_nullary_atoms(
+      const std::unordered_set<int> &indices,
+      const std::vector<bool> &nullary_atoms);
+  int atom_not_satisfied(const State &s, const AtomicGoal &atomicGoal) const;
+};
 
 #endif //SEARCH_GOALCOUNT_H
