c221b996a0fdaaa53475f44a94694e7ccf4efb6b
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-04-14 15:33:40 +0200

    Pass function declaration from parser.c to parser.h

--- src/search/parser.h
+++ src/search/parser.h
@@ -8,4 +8,18 @@
 bool parse(Task &task, const std::ifstream &in);
 void output_error(string &msg);
 
+bool is_sparse_representation(string &canary);
+bool is_next_section_correct(string &canary, const string& expected);
+/*
+ * Read next n values from stdin and copy to v
+ */
+void copy_next_n_values(int n, vector<int> &v);
+
+void parse_types(Task &task, int number_types);
+void parse_predicates(Task &task, int number_predicates);
+void parse_objects(Task &task, int number_objects);
+void parse_initial_state(Task &task, int initial_state_size);
+void parse_goal(Task &task, int goal_size);
+void parse_action_schemas(Task &task, int number_action_schemas);
+
 #endif // SEARCH_PARSER_H
