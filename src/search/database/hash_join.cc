d8b2ac4756ac417123d41f1dc4004dafe4e17b6e
Guillem Frances <guillem.frances@gmail.com>
2020-04-28 18:43:36 +0200

    Refactor hash_join

--- src/search/database/hash_join.cc
+++ src/search/database/hash_join.cc
@@ -3,9 +3,22 @@
 #include "utils.h"
 
 #include <algorithm>
+#include <cassert>
 #include <unordered_map>
 
 using namespace std;
+
+std::vector<int> project_tuple(
+    const std::vector<int>& tuple,
+    const std::vector<int>& pattern)
+{
+    auto sz = pattern.size();
+    vector<int> projected(sz);
+    for (size_t i = 0; i < sz; ++i) {
+        projected[i] = tuple[pattern[i]];
+    }
+    return projected;
+}
 
 void hash_join(Table &t1, const Table &t2) {
     /*
@@ -17,10 +30,12 @@ void hash_join(Table &t1, const Table &t2) {
      *    matching keys. Then, loop over the second table searching for hits
      *    in the hash table.
      */
-    auto matches = compute_matching_columns(t1, t2);
+    std::vector<int> matches1, matches2;
+    compute_matching_columns(t1, t2, matches1, matches2);
+    assert(matches1.size()==matches2.size());
 
     vector<vector<int>> new_tuples;
-    if (matches.empty()) {
+    if (matches1.empty()) {
         /*
          * If no attribute matches, then we apply a cartesian product
          * TODO this code is duplicate from join.cc, make it an auxiliary function
@@ -35,21 +50,18 @@ void hash_join(Table &t1, const Table &t2) {
         }
     }
     else {
-        unordered_map<vector<int>, unordered_set<vector<int>, TupleHash>, TupleHash> hash_join_map;
+        unordered_map<vector<int>, vector<vector<int>>, TupleHash> hash_join_map;
         // Build phase
         for (const vector<int> &tuple : t1.tuples) {
-            vector<int> key(matches.size());
-            for (size_t i = 0; i < matches.size(); i++) {
-                key[i] = tuple[matches[i].first];
-            }
-            hash_join_map[key].insert(tuple);
+            hash_join_map[project_tuple(tuple, matches1)].push_back(tuple);
         }
 
         // Remove duplicated index. Duplicate code from join.cc
         vector<bool> to_remove(t2.tuple_index.size(), false);
-        for (const pair<int, int> &m : matches) {
-            to_remove[m.second] = true;
+        for (const auto &m : matches2) {
+            to_remove[m] = true;
         }
+
         for (size_t j = 0; j < t2.tuple_index.size(); ++j) {
             if (!to_remove[j]) {
                 t1.tuple_index.push_back(t2.tuple_index[j]);
@@ -58,21 +70,17 @@ void hash_join(Table &t1, const Table &t2) {
 
         // Probe phase
         for (vector<int> tuple : t2.tuples) {
-            vector<int> key(matches.size());
-            for (size_t i = 0; i < matches.size(); i++) {
-                key[i] = tuple[matches[i].second];
-            }
-            if (hash_join_map.count(key) > 0) {
-                for (int i = to_remove.size()-1; i >= 0 ; --i) {
-                    if (to_remove[i]) {
-                        tuple.erase(tuple.begin() + i);
+
+            auto it = hash_join_map.find(project_tuple(tuple, matches2));
+
+            if (it != hash_join_map.end()) {
+                const auto& matching_tuples = it->second;
+                for (vector<int> t:matching_tuples) {
+                    for (unsigned j = 0; j < to_remove.size(); ++j) {
+                        if (!to_remove[j]) t.push_back(tuple[j]);
                     }
-                }
-                for (vector<int> t : hash_join_map[key]) {
-                    t.insert(t.end(), tuple.begin(), tuple.end());
                     new_tuples.push_back(std::move(t));
                 }
-
             }
         }
 
