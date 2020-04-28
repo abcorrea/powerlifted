4b09254b64186cc018b64b5e6015aa52c4e29dae
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-04-28 15:22:49 +0200

    Correct project operator

--- src/search/database/project.cc
+++ src/search/database/project.cc
@@ -8,24 +8,26 @@ using namespace std;
 void project(Table &t, const std::unordered_set<int> &over) {
 
     vector<int> matches;
-    for (int x : over)
-        for (size_t i = 0; i < t.tuple_index.size(); i++)
-            if (x == t.tuple_index[i])
+    for (int x : over) {
+        for (size_t i = 0; i < t.tuple_index.size(); i++) {
+            if (x==t.tuple_index[i])
                 matches.push_back(i);
+        }
+    }
 
-    unordered_set<vector<int>, TupleHash> projected_tuples;
+    unordered_map<vector<int>, vector<int>, TupleHash> hash_map;
     for (const vector<int> &tuple : t.tuples) {
         vector<int> key(matches.size());
         for (size_t i = 0; i < matches.size(); i++) {
                 key[i] = tuple[matches[i]];
             }
-        projected_tuples.insert(tuple);
+        if (hash_map.count(key) == 0)
+            hash_map[key] = tuple;
     }
 
     unordered_set<vector<int>, TupleHash> new_tuples;
-    for (const auto& tup: projected_tuples) {
-        new_tuples.insert(tup);
+    for (const auto& entry: hash_map) {
+        new_tuples.insert(entry.second);
     }
     t.tuples = new_tuples;
-
 }
