706ed91b873735f96006771ec73f3f4fac4a336d
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-04-30 20:44:47 +0200

    Correct projection (again) and add comment

--- src/search/database/project.cc
+++ src/search/database/project.cc
@@ -2,33 +2,40 @@
 #include "project.h"
 #include "table.h"
 
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 void project(Table &t, const std::unordered_set<int> &over) {
 
-    vector<int> matches(over.size());
-    vector<int> new_indices(over.size());
-    int counter = 0 ;
-    for (int x : over) {
-        new_indices[counter] = x;
-        for (size_t i = 0; i < t.tuple_index.size(); i++) {
-            if (x==t.tuple_index[i])
-                matches[counter++] = i;
-        }
-    }
+    /*
+     * This projection is not a canonical projection. We still need to keep *a full assignment*
+     * for every column of the table so we can instantiate the action later on. Thus, we DO NOT
+     * remove the indices from the table here.
+     */
 
-    vector<vector<int>> new_tuples;
+    vector<int> matches;
+    for (int x : over)
+        for (size_t i = 0; i < t.tuple_index.size(); i++)
+            if (x == t.tuple_index[i])
+                matches.push_back(i);
+
+    unordered_map<vector<int>, vector<int>, TupleHash> hash_map;
+
     for (const vector<int> &tuple : t.tuples) {
         vector<int> key(matches.size());
         for (size_t i = 0; i < matches.size(); i++) {
                 key[i] = tuple[matches[i]];
-        }
-        new_tuples.push_back(std::move(key));
+            }
+        if (hash_map.count(key) == 0)
+            hash_map[key] = tuple;
     }
 
-    t.tuples = std::move(new_tuples);
-    t.tuple_index = std::move(new_indices);
+    vector<vector<int>> new_tuples;
+    new_tuples.reserve(hash_map.size());
+    for (const auto &entry : hash_map) {
+        new_tuples.push_back(entry.second);
+    }
+    t.tuples = new_tuples;
 }
