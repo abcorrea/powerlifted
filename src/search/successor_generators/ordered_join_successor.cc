ea4dcca39c4dffd0f21dc0cb27d7362bdb8f7881
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-06-22 14:35:41 +0200

    Preprocess join order for arity-based joins

--- src/search/successor_generators/ordered_join_successor.cc
+++ src/search/successor_generators/ordered_join_successor.cc
@@ -1,8 +1,13 @@
 #include "ordered_join_successor.h"
+
 #include "../action.h"
+#include "../task.h"
+
 #include "../database/table.h"
+#include "../database/hash_join.h"
 #include "../states/state.h"
 
+#include <cassert>
 #include <vector>
 
 using namespace std;
@@ -16,14 +21,70 @@ bool OrderedJoinSuccessorGenerator<OrderT>::parse_precond_into_join_program(
     bool res = GenericJoinSuccessor::parse_precond_into_join_program(adata, state, tables);
     if (!res) return false;
 
-    std::sort(tables.begin(), tables.end(), OrderT());
     return true;
 }
 
 template <typename OrderT>
 OrderedJoinSuccessorGenerator<OrderT>::OrderedJoinSuccessorGenerator(const Task &task)
-    : GenericJoinSuccessor(task)
-{
+    : GenericJoinSuccessor(task) {
+    vector<pair<int,int>> to_sort;
+    int a_idx = 0;
+    assert(action_data.size() == task.actions.size());
+    precondition_to_order.resize(action_data.size());
+    for (auto &a : action_data) {
+        to_sort.clear();
+        int k = 0;
+        for (const auto & i : a.precompiled_db) {
+            /*const Atom &atom = a.relevant_precondition_atoms[i];
+            int n_free_vars = 0;
+            for (const auto &c : atom.arguments)
+                if (!c.constant) ++n_free_vars;*/
+            to_sort.emplace_back(i.tuple_index.size(), k++);
+        }
+        std::sort(to_sort.begin(), to_sort.end(), OrderT());
+
+        int _counter = 0;
+        for (auto p : to_sort) {
+            int idx = p.second;
+            precondition_to_order[a_idx][_counter++] = idx;
+        }
+        ++a_idx;
+    }
+}
+
+template<typename OrderT>
+Table OrderedJoinSuccessorGenerator<OrderT>::instantiate(const ActionSchema &action,
+                                                         const DBState &state) {
+
+    map<int, int> order = precondition_to_order[action.get_index()];
+
+    if (action.is_ground()) {
+        throw std::runtime_error("Shouldn't be calling instantiate() on a ground action");
+    }
+
+    const auto& actiondata = action_data[action.get_index()];
+
+    vector<Table> tables(0);
+    auto res = parse_precond_into_join_program(actiondata, state, tables);
+
+    if (!res) return Table::EMPTY_TABLE();
+
+    assert(!tables.empty());
+    assert(tables.size() == actiondata.relevant_precondition_atoms.size());
+
+    Table &working_table = tables[order[0]];
+    for (size_t i = 1; i < tables.size(); ++i) {
+        hash_join(working_table, tables[order[i]]);
+        // Filter out equalities
+        filter_inequalities(action, working_table);
+        if (working_table.tuples.empty()) {
+            return working_table;
+        }
+    }
+
+    return working_table;
+
+    return GenericJoinSuccessor::instantiate(action, state);
 }
 
 // explicit template instantiations
