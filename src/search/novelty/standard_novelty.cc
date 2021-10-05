a7a9a4ecee2d19d1bfaea41f3be34f4e9273b2cb
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2021-10-05 17:45:05 +0200

    Refactoring

--- src/search/novelty/standard_novelty.cc
+++ src/search/novelty/standard_novelty.cc
@@ -1,2 +1,36 @@
 #include "standard_novelty.h"
 
+int StandardNovelty::compute_novelty(const Task &task, const DBState &state) {
+    int number_unsatisfied_goals = gc.compute_heuristic(state, task);
+    if (number_unsatisfied_goals == 0) {
+        return GOAL;
+    }
+    int idx = number_unsatisfied_goals - 1;
+
+    auto &achieved_atoms_in_layer = achieved_atoms[idx];
+
+    bool has_novel_atom = false;
+    for (const Relation &relation : state.get_relations()) {
+        int pred_symbol_idx = relation.predicate_symbol;
+        for (const GroundAtom& tuple : relation.tuples) {
+            bool is_new = achieved_atoms_in_layer.try_to_insert(pred_symbol_idx, tuple);
+            if (is_new)
+                has_novel_atom = true;
+        }
+    }
+
+    const std::vector<bool>& nullary_atoms = state.get_nullary_atoms();
+    for (size_t i = 0; i < nullary_atoms.size(); ++i) {
+        if (nullary_atoms[i]) {
+            bool is_new = achieved_atoms_in_layer.try_to_insert_nullary_atom(i);
+            if (is_new)
+                has_novel_atom = true;
+        }
+    }
+
+    if (has_novel_atom)
+        return NOVEL;
+    else
+        return NOT_NOVEL;
+
+}
--- src/search/novelty/standard_novelty.h
+++ src/search/novelty/standard_novelty.h
@@ -25,14 +25,6 @@ public:
         nullary_atoms.resize(task.initial_state.get_nullary_atoms().size(), false);
     }
 
-    bool empty() {
-        return ground_atoms.empty();
-    }
-
-    void insert(int i, const GroundAtom& ga) {
-        ground_atoms.emplace(i, ga);
-    }
-
     bool try_to_insert(int i, const GroundAtom& ga) {
         auto it = ground_atoms.insert(make_pair(i, ga));
         return it.second;
@@ -46,12 +38,17 @@ public:
 
 };
 
+
+/*
+ *
+ * This implements the evaluator R_0 from Frances et al (IJCAI-17) -- assuming it is combined
+ * with the BFWS class.
+ *
+ */
 class StandardNovelty {
 
     Goalcount gc;
     std::vector<AchievedGroundAtoms> achieved_atoms;
-
-
 
 public:
 
@@ -68,41 +65,7 @@ public:
 
     }
 
-    int compute_novelty(const Task &task, const DBState &state) {
-        int number_unsatisfied_goals = gc.compute_heuristic(state, task);
-        if (number_unsatisfied_goals == 0) {
-            return GOAL;
-        }
-        int idx = number_unsatisfied_goals - 1;
-
-        auto &achieved_atoms_in_layer = achieved_atoms[idx];
-
-        bool has_novel_atom = false;
-        for (const Relation &relation : state.get_relations()) {
-            int pred_symbol_idx = relation.predicate_symbol;
-            for (const GroundAtom& tuple : relation.tuples) {
-                bool is_new = achieved_atoms_in_layer.try_to_insert(pred_symbol_idx,
-                                                                tuple);
-                if (is_new)
-                    has_novel_atom = true;
-            }
-        }
-
-        const std::vector<bool>& nullary_atoms = state.get_nullary_atoms();
-        for (size_t i = 0; i < nullary_atoms.size(); ++i) {
-            if (nullary_atoms[i]) {
-                bool is_new = achieved_atoms_in_layer.try_to_insert_nullary_atom(i);
-                if (is_new)
-                    has_novel_atom = true;
-            }
-        }
-
-        if (has_novel_atom)
-            return NOVEL;
-        else
-            return NOT_NOVEL;
-
-    }
+    int compute_novelty(const Task &task, const DBState &state);
 
 };
 
