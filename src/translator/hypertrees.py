575838ddd65bc727acb61ade82b0bd8837245f86
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2021-10-15 18:03:55 +0200

    Add Hypertree class and parse hypertrees from output

--- src/translator/hypertrees.py
+++ src/translator/hypertrees.py
@@ -4,6 +4,84 @@ import os
 import subprocess
 
 
+class Hypertree:
+    def __init__(self) -> None:
+        self.bag = []
+        self.cover = []
+        self.parent = None
+        self.children = []
+
+    def set_bag(self, vertices):
+        self.bag = vertices
+
+    def set_cover(self, edges):
+        self.cover = edges
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def del_child(self, child):
+        self.children.remove(child)
+        child.bag = None
+        child.cover = None
+        child.parent = None
+        child.children = None
+
+    def _upwards(self):
+        if self.parent is not None:
+            p = self.parent
+            if subset(self.bag, p.bag):
+                p.cover.extend(self.cover)
+                p.cover = list(dict.fromkeys(p.cover))
+                for c in self.children:
+                    p.add_child(c)
+
+                p.del_child(self)
+                return True
+        return False
+
+    def _downwards(self):
+        heir = None
+        for c in self.children:
+            if subset(self.bag, c.bag):
+                heir = c
+                break
+        if heir is not None:
+            heir.cover.extend(self.cover)
+            heir.cover = list(dict.fromkeys(heir.cover))
+            self.children.remove(heir)
+            for s in self.children:
+                heir.add_child(s)
+            if self.parent is not None:
+                self.parent.add_child(heir)
+                self.parent.del_child(self)
+            else:
+                heir.parent = None
+                self.children = None
+            return True
+        return False
+
+
+class Hypergraph:
+    def __init__(self) -> None:
+        self.edges = {}
+
+    def add_edge(self, name, vertices):
+        self.edges[name] = vertices
+
+
+'''l1 subset l2'''
+
+
+def subset(l1, l2):
+    for e in l1:
+        if e not in l2:
+            return False
+    return True
+
+
+
 def get_hypertree_decompositions(task):
     print("Using Hypertree decompositions. 'BalancedGo' is expected to be in the PATH.")
     delete_previous_htd_files()
@@ -41,12 +119,37 @@ def generate_action_hypertree(action, i):
 
 
 def compute_decompositions(file):
-    import sys
-    cwd = os.getcwd()
-    abs_path = os.path.join(cwd, file)
-
     decomp_file_name = file
     decomp_file_name = decomp_file_name.replace('.ast', '.htd')
     f = open(decomp_file_name, 'w')
-    BALANCED_GO_CMD = ['BalancedGo', '-bench', '-approx', '10', '-det', '-graph', abs_path]
-    subprocess.call(BALANCED_GO_CMD, stdout=f)
+    BALANCED_GO_CMD = ['BalancedGo',
+                       '-bench',
+                       '-approx', '10',
+                       '-det',
+                       '-graph', file,
+                       '-complete',
+                       '-gml', decomp_file_name]
+
+    res = subprocess.run(BALANCED_GO_CMD, stdout=subprocess.PIPE,
+                         check=True, universal_newlines=True)
+    hd = []
+    parents = []
+    for line in res.stdout.splitlines():
+        if 'Bag: {' in line:
+            node = Hypertree()
+            line = line.strip()[6:-1]
+            node.set_bag([v.strip() for v in line.split(',')])
+            hd.append(node)
+
+            if len(parents):
+                par = parents[-1]
+                par.add_child(node)
+        elif 'Cover: {' in line:
+            line = line.strip()[8:-1]
+            hd[-1].set_cover([v.strip() for v in line.split(',')])
+            #hd[-1].covered = covered(hd[-1]) # Davide told to comment out this list
+        elif 'Children:' in line:
+            parents.append(hd[-1])
+        elif ']' in line:
+            parents = parents[:-1]
+    return hd
