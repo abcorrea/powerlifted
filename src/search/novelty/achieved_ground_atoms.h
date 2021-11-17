e9410114db1b7cef6b37f608e496b3f938745f08
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2021-11-18 00:43:09 +0100

    Move AchievedGroundAtoms to its own file

---
+++ src/search/novelty/achieved_ground_atoms.h
@@ -0,0 +1,39 @@
+#ifndef SEARCH_NOVELTY_ACHIEVED_GROUND_ATOMS_H_
+#define SEARCH_NOVELTY_ACHIEVED_GROUND_ATOMS_H_
+
+#include "../task.h"
+
+class AchievedGroundAtoms {
+
+    // TODO Check small vector optimizations.
+    /* TODO Check also ArrayPool class (Scorpion).
+     * Use ArrayPool to store vectors and each entry could have only the index to it.
+     */
+    std::vector<absl::flat_hash_set<int>> ground_atoms_k1;
+
+    std::vector<absl::flat_hash_set<std::pair<int, int>>> ground_atoms_k2;
+
+public:
+
+    AchievedGroundAtoms() = default;
+
+    AchievedGroundAtoms(const Task &task, size_t number_combinations) :
+        ground_atoms_k1(task.initial_state.get_relations().size()) {
+        // TODO Initialize only if width=2
+        ground_atoms_k2.resize(number_combinations);
+    }
+
+    bool try_to_insert_atom_in_k1(int i, int idx) {
+        auto it = ground_atoms_k1[i].insert(idx);
+        return it.second;
+    }
+
+    bool try_to_insert_atom_in_k2(int idx, int idx_ga1, int idx_ga2) {
+        auto it = ground_atoms_k2[idx].insert({idx_ga1, idx_ga2});
+        return it.second;
+    }
+
+};
+
+
+#endif //SEARCH_NOVELTY_ACHIEVED_GROUND_ATOMS_H_
--- src/search/novelty/standard_novelty.h
+++ src/search/novelty/standard_novelty.h
@@ -8,6 +8,8 @@
 #include <boost/functional/hash.hpp>
 #include <boost/container/small_vector.hpp>
 
+#include "achieved_ground_atoms.h"
+
 #include "../task.h"
 
 #include "../heuristics/goalcount.h"
@@ -15,38 +17,6 @@
 #include "../structures.h"
 
 typedef absl::flat_hash_map<GroundAtom, int> NoveltySet;
-
-class AchievedGroundAtoms {
-
-    // TODO Check small vector optimizations.
-    /* TODO Check also ArrayPool class (Scorpion).
-     * Use ArrayPool to store vectors and each entry could have only the index to it.
-     */
-    std::vector<absl::flat_hash_set<int>> ground_atoms_k1;
-
-    std::vector<absl::flat_hash_set<std::pair<int, int>>> ground_atoms_k2;
-
-public:
-
-    AchievedGroundAtoms() = default;
-
-    AchievedGroundAtoms(const Task &task, size_t number_combinations) :
-    ground_atoms_k1(task.initial_state.get_relations().size()) {
-        // TODO Initialize only if width=2
-        ground_atoms_k2.resize(number_combinations);
-    }
-
-    bool try_to_insert_atom_in_k1(int i, int idx) {
-        auto it = ground_atoms_k1[i].insert(idx);
-        return it.second;
-    }
-
-    bool try_to_insert_atom_in_k2(int idx, int idx_ga1, int idx_ga2) {
-        auto it = ground_atoms_k2[idx].insert({idx_ga1, idx_ga2});
-        return it.second;
-    }
-
-};
 
 
 /*
