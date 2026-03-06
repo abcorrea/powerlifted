ebba138da80497d4180d537e74f87da04b06d896
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2026-03-06 15:24:38 +0000

    Refactor successor generator joins

--- src/search/database/utils.cc
+++ src/search/database/utils.cc
@@ -19,7 +19,6 @@ std::vector<std::pair<int, int>> compute_matching_columns(const Table &t1, const
 }
 
 void compute_matching_columns(const Table &t1, const Table &t2, std::vector<int>& matches1, std::vector<int>& matches2) {
-    vector<pair<int, int>> matches;
     auto sz1 = t1.tuple_index.size();
     auto sz2 = t2.tuple_index.size();
     for (size_t i = 0; i < sz1; ++i) {
@@ -30,5 +29,28 @@ void compute_matching_columns(const Table &t1, const Table &t2, std::vector<int>
             }
         }
     }
+}
+
+void cartesian_product(Table &t1, const Table &t2) {
+    t1.tuple_index.insert(t1.tuple_index.end(),
+                          t2.tuple_index.begin(), t2.tuple_index.end());
+
+    const size_t sz1 = t1.tuples.empty() ? 0 : t1.tuples[0].size();
+    const size_t sz2 = t2.tuples.empty() ? 0 : t2.tuples[0].size();
+    const size_t combined_sz = sz1 + sz2;
+
+    vector<vector<int>> new_tuples;
+    new_tuples.reserve(t1.tuples.size() * t2.tuples.size());
+
+    for (const auto &tuple_t1 : t1.tuples) {
+        for (const auto &tuple_t2 : t2.tuples) {
+            vector<int> combined;
+            combined.reserve(combined_sz);
+            combined.insert(combined.end(), tuple_t1.begin(), tuple_t1.end());
+            combined.insert(combined.end(), tuple_t2.begin(), tuple_t2.end());
+            new_tuples.push_back(std::move(combined));
+        }
+    }
+    t1.tuples = std::move(new_tuples);
 }
 
