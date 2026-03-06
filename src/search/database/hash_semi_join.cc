de14f5d8344bd536558083a236c840344d638189
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2026-03-06 15:24:38 +0000

    Refactor successor generator joins

--- src/search/database/hash_semi_join.cc
+++ src/search/database/hash_semi_join.cc
@@ -4,9 +4,7 @@
 #include "table.h"
 #include "utils.h"
 
-#include <unordered_map>
 #include <unordered_set>
-#include <utility>
 #include <vector>
 
 using namespace std;
@@ -14,41 +12,36 @@ using namespace std;
 size_t hash_semi_join(Table &t1, const Table &t2) {
     auto matches = compute_matching_columns(t1, t2);
 
-    vector<vector<int>> new_tuples;
     if (matches.empty()) {
-        /*
-         * If no attribute matches, then we return
-         */
         return t1.tuples.size();
     }
-    else {
-        /*
-         * Otherwise, we perform the join and the projection
-         */
-        unordered_map<vector<int>, unordered_set<vector<int>, TupleHash>, TupleHash> hash_join_map;
-        // Build phase
-        for (const vector<int> &tuple : t2.tuples) {
-            vector<int> key(matches.size());
-            for (size_t i = 0; i < matches.size(); i++) {
+
+    // Build phase: collect the set of join keys present in t2
+    unordered_set<vector<int>, TupleHash> t2_keys;
+    {
+        vector<int> key(matches.size());
+        for (const auto &tuple : t2.tuples) {
+            for (size_t i = 0; i < matches.size(); ++i) {
                 key[i] = tuple[matches[i].second];
             }
-            hash_join_map[key].insert(tuple);
+            t2_keys.insert(key);
         }
+    }
 
-        for (const vector<int> &tuple : t1.tuples) {
-            vector<int> key(matches.size());
-            for (size_t i = 0; i < matches.size(); i++) {
+    // Probe phase: keep t1 tuples whose join key exists in t2
+    vector<vector<int>> new_tuples;
+    {
+        vector<int> key(matches.size());
+        for (const auto &tuple : t1.tuples) {
+            for (size_t i = 0; i < matches.size(); ++i) {
                 key[i] = tuple[matches[i].first];
             }
-            if (hash_join_map.count(key) > 0) {
+            if (t2_keys.count(key) > 0) {
                 new_tuples.push_back(tuple);
             }
         }
     }
+
     t1.tuples = std::move(new_tuples);
     return t1.tuples.size();
 }
-
-
-
-
--- src/search/database/join.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEARCH_JOIN_H
-#define SEARCH_JOIN_H
-
-class Table;
-
-void join(Table &t1, Table &t2);
-
-
-#endif //SEARCH_JOIN_H
